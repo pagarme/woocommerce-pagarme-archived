1a00288d70a21097c826dfdaad86e411956d9947
Leonam Pereira Dias <leonam.pd@gmail.com>
2019-04-05 18:52:26 -0300

    finishing poc

--- tests/e2e/credit_card.spec.js
+++ tests/e2e/credit_card.spec.js
@@ -1,22 +1,81 @@
-describe( 'Credit card', () => {
-  // beforeEach( function() {
-  // })
+import checkoutData from './fixtures/data';
 
-  it('Add to Cart', () => {
+context('Credit card', () => {
+  before(() => {
     cy.visit('/product/hoodie-with-zipper/')
 
-    cy.contains('Hoodie with Zipper')
     cy.contains('Comprar')
       .click()
 
     cy.contains('Ver carrinho')
       .click()
 
-    cy.contains('Hoodie with Zipper')
     cy.contains('Fechar compra')
       .click()
 
-    // cy.get('form')
-    //   .submit()
+    it('should be redirected to checkout page', () => {
+      cy.url().should('include', '/finalizar-compra')
+      cy.contains('Detalhes de cobrança')
+    })
+  })
+
+  describe('Basic purchase workflow', () => {
+
+    it('should be redirected to checkout page', () => {
+      cy
+        .get('#billing_first_name')
+        .type(checkoutData.customer.name)
+        .should("have.value", checkoutData.customer.name)
+        
+      cy
+        .get('#billing_last_name')
+        .type(checkoutData.customer.lastname)
+        .should("have.value", checkoutData.customer.lastname)
+        
+      cy
+        .get('#billing_cpf')
+        .type(checkoutData.customer.documents[0].number)
+        .should("have.value", "306.211.430-49")
+
+      cy
+        .get('#billing_postcode')
+        .type(checkoutData.address.zipcode)
+        .should('have.value', checkoutData.address.zipcode)
+
+      cy
+        .get('#billing_address_1')
+        .type(checkoutData.address.street)
+        .should('have.value', checkoutData.address.street)
+        
+      cy
+        .get('#billing_number')
+        .type(checkoutData.address.street_number)
+        .should('have.value', checkoutData.address.street_number)
+
+      cy
+        .get('#billing_neighborhood')
+        .type(checkoutData.address.neighborhood)
+        .should('have.value', checkoutData.address.neighborhood)
+
+      cy
+        .get('#billing_city')
+        .type(checkoutData.address.city)
+        .should('have.value',checkoutData.address.city)
+
+      cy
+        .get('#billing_phone')
+        .type(checkoutData.customer.phone_numbers[0])
+        .should('have.value', checkoutData.customer.phone_numbers[0])
+
+      cy
+        .get('#billing_cellphone')
+        .type(checkoutData.customer.phone_numbers[0])
+        .should('have.value', checkoutData.customer.phone_numbers[0])
+
+      cy
+        .get('#billing_email')
+        .type(checkoutData.customer.email)
+        .should("have.value", checkoutData.customer.email)
+    })
   })
 })
